5d4738647a4f20bb5db5cec65d16ee88f410cd7f
Rodrigo Rech <37411591+RodrigoR5@users.noreply.github.com>
2019-11-08 13:31:43 -0300

    pesquisando idade no google e descobrindo signo

---
+++ HoroscopoBot/src/webNavegator/Controller.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webNavegator;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Controller {
+
+        public static void main(String[] args) {
+            
+            searchEngine a = new searchEngine();
+            a.procuraIdade("Roberto Carlos");
+        }
+
+}
