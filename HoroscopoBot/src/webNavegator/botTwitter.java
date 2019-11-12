b77fabe61f485519bc36849d90308b93d19e9d85
Rodrigo Rech <37411591+RodrigoR5@users.noreply.github.com>
2019-11-12 15:55:53 -0300

    Inclusão do Json para analisar as mentions

--- HoroscopoBot/src/webNavegator/botTwitter.java
+++ HoroscopoBot/src/webNavegator/botTwitter.java
@@ -7,6 +7,7 @@ package webNavegator;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import org.json.JSONObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -28,7 +29,21 @@ public class botTwitter extends browserManager{
         element = driver.findElement(By.cssSelector(".js-password-field"));
         element.sendKeys("password");
         element.submit();
-        driver.get("https://twitter.com/notifications/mentions");
+        driver.get("https://twitter.com/notifications/mentions"); 
+        
+        List<WebElement> elements = driver.findElements(By.cssSelector("[class = \"css-4rbku5 css-18t94o4 css-901oao r-111h2gw r-1loqt21 r-1q142lx r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-3s2u2q r-qvutc0\"]"));
+
+        JSONObject mentions = new JSONObject();
+        
+        for(int i = 0; i<elements.size(); i++){//adiciona cada mention encontrada no json, caso não esteja
+            String idTwitter = elements.get(i).getAttribute("href");
+            if(!mentions.has(idTwitter))    {
+                /*Entra no if caso a menção não esteja no json*/
+                mentions.put(idTwitter, "false");
+            }
+        }
+        System.out.println(mentions.toString());       
+
         return driver;
     }
 }
