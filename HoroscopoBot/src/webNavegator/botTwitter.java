f1c64b1c5cee111bf317d71f89638142730af77f
Rodrigo Rech <rodrigorech@hotmail.com>
2019-11-27 00:59:20 -0300

    Salva o json no disco
    Adicionei a função writeJsonTxt e readJsonTxt, elas gravam o json no disco

--- HoroscopoBot/src/webNavegator/botTwitter.java
+++ HoroscopoBot/src/webNavegator/botTwitter.java
@@ -5,6 +5,11 @@
  */
 package webNavegator;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.json.JSONArray;
@@ -12,14 +17,13 @@ import org.json.JSONObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 /**
  *
  * @author Rodrigo
  */
 public class botTwitter extends browserManager{
     
-    public WebDriver login(){
+    public WebDriver login() throws IOException{
         /*O método abre o navegador e efetua o login no twitter*/
         WebDriver driver = super.openChrome();
         driver.get("http://www.twitter.com/login");
@@ -33,19 +37,41 @@ public class botTwitter extends browserManager{
         driver.get("https://twitter.com/notifications/mentions"); 
         
         List<WebElement> elements = driver.findElements(By.cssSelector("[class = \"css-4rbku5 css-18t94o4 css-901oao r-111h2gw r-1loqt21 r-1q142lx r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-3s2u2q r-qvutc0\"]"));
-        JSONObject mentions = new JSONObject();
+        JSONObject mentions = createJson(elements);
+        this.writeJsonTxt(mentions);
+        this.readJsonTxt();
+        return driver;
+    }
+    
+    private JSONObject createJson(List<WebElement> elements) throws IOException{
+        JSONObject mentions = new JSONObject(this.readJsonTxt());
         JSONArray array = new JSONArray();
         for(int i = 0; i<elements.size(); i++){
             String linkTwitter = elements.get(i).getAttribute("href");
             array = this.updateArray(array, linkTwitter);
         }
-        //mentions = this.updateJson(elements, mentions);
         mentions.put("mentions", array);
-        System.out.println(mentions.toString());
-        return driver;
+        
+        return mentions;
     }
     
+    /*
+    Escreve no json.txt um JSONObject recebido
+    */
+    private void writeJsonTxt(JSONObject mentions) throws IOException{
+        String mentionsString = mentions.toString();
+        try (FileWriter save = new FileWriter("json.txt")) {
+            save.write(mentionsString);
+        }
+    }
 
+    private String readJsonTxt() throws FileNotFoundException, IOException{
+        FileReader arqLeitura = new FileReader("json.txt");
+        BufferedReader buffer = new BufferedReader(arqLeitura);
+        String readText = buffer.readLine();
+	return readText;
+    }
+            
     private JSONArray updateArray(JSONArray mentions, String link){
         for(int i=0; i < mentions.length(); i++){
             JSONObject temporaryMention = new JSONObject(mentions.get(i));
@@ -58,9 +84,6 @@ public class botTwitter extends browserManager{
         mentions.put(newMention);
         return mentions;
     }
-    
-//    public void searchJsonWithFalse(JSONObject mentions){
-//        System.out.println(mentions.optNumber(1));
-//    }
+   
 
 }
