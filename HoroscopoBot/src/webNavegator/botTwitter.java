b1526c16066eb154d3f9f7aaf3e548f5d99c8847
Rodrigo Rech <rodrigorech@hotmail.com>
2019-11-28 23:03:00 -0300

    Desfazendo refatoração
    A ultima refatoração acabou não sendo produtiva, então voltei atrás em alguns pontos

--- HoroscopoBot/src/webNavegator/botTwitter.java
+++ HoroscopoBot/src/webNavegator/botTwitter.java
@@ -23,7 +23,6 @@ import org.openqa.selenium.WebElement;
  */
 public class botTwitter extends browserManager{
     
-    private JSONObject mentions;
     private WebDriver driver;
     
     public void login(){
@@ -33,38 +32,48 @@ public class botTwitter extends browserManager{
         this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement element;
         element = this.driver.findElement(By.cssSelector(".js-username-field"));
-        element.sendKeys("user");
+        element.sendKeys("digaorech@hotmail.com");
         element = this.driver.findElement(By.cssSelector(".js-password-field"));
-        element.sendKeys("password");
+        element.sendKeys("ciamirim5");
         element.submit();
     }
-    
+    /*
+    Este método só pode ser realizado após o login
+    */
     public void searchForQuestions() throws IOException {
         this.driver.get("https://twitter.com/notifications/mentions"); 
         List<WebElement> elements = this.driver.findElements(By.cssSelector("[class = \"css-4rbku5 css-18t94o4 css-901oao r-111h2gw r-1loqt21 r-1q142lx r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-3s2u2q r-qvutc0\"]"));
-        this.createJson(elements);
+        JSONObject mentions = this.createJson(elements);
         
-        this.writeJsonInDisc();
+        this.writeJsonInDisc(mentions);
     }
-    private void createJson(List<WebElement> elements) throws IOException{
-        JSONObject mentionsTemporary = new JSONObject(this.readJsonToDisc());
+    private JSONObject createJson(List<WebElement> elements) throws IOException{
+        String rd = this.readJsonToDisc();
+        JSONObject mentionsTemporary;
+        if(rd != null){
+            mentionsTemporary = new JSONObject(rd);
+        }
+        else{
+            mentionsTemporary = new JSONObject();
+        }
         JSONArray array = new JSONArray();
+  
+
         for(int i = 0; i<elements.size(); i++){
             String linkTwitter = elements.get(i).getAttribute("href");
             array = this.updateArray(array, linkTwitter);
         }
         mentionsTemporary.put("mentions", array);
-        
-        this.mentions = mentionsTemporary;
+        return mentionsTemporary;
     }
-    
+        
     /*
     Escreve no json.txt um JSONObject recebido
     */
-    private void writeJsonInDisc() throws IOException{
-        String mentionsString = this.mentions.toString();
+    private void writeJsonInDisc(JSONObject mentions) throws IOException{
+        
         try (FileWriter save = new FileWriter("json.txt")) {
-            save.write(mentionsString);
+            save.write(mentions.toString());
         }
     }
 
@@ -88,5 +97,4 @@ public class botTwitter extends browserManager{
         return mentions;
     }
    
-
 }
